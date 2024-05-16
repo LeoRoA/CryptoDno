527b328cbcd7c939de96d399c4a47d5606169ce9
LeoRoA <romanleontev@gmail.com>
2024-05-16 10:18:35 +0700

    Initial commit

---
+++ src/main/java/com/homecase/cryptodno/CryptoDnoApplication.java
@@ -0,0 +1,13 @@
+package com.homecase.cryptodno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoDnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoDnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homecase/cryptodno/CryptoDnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecase.cryptodno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoDnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
